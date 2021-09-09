fdba107a72249613e99884eb2bc5b49c735bfa7a
tech-keny <chanken7@icloud.com>
2021-09-09 16:11:33 +0900

    9/9更新

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,5 @@ from app import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('', views.StoreView.as_view(), name='store'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,17 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render
+from app.models import Store
 
 
 class IndexView(LoginRequiredMixin, TemplateView):
     template_name = "app/index.html"
     login_url = '/accounts/login/'
+
+class StoreView(View):
+    def get(self, request, *args, **kwargs):
+        store_data = Store.objects.all()
+
+        return render(request, 'app/store.html', {
+            'store_data': store_data,
+        })
